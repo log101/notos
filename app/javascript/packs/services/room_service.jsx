eac9269570725cd4ae50e055100bffd2eed00c86
log101 <frknerdm@mail.com>
2022-07-10 11:51:55 +0300

    change base url

--- app/javascript/packs/services/room_service.jsx
+++ app/javascript/packs/services/room_service.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = "http://localhost:3000/room"
+const baseUrl = "http://127.0.0.1/room"
 
 const getRoom = () => {
     const req = axios.get(`${baseUrl}/get`)
